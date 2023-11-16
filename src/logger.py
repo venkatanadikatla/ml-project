37a13426f620bd2e8d100db0a3aa788d3bc25a4b
Venkat <nvenkatavijay@gmail.com>
2023-11-16 15:23:49 -0500

    datasets

---
+++ src/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+#need to provide the path for log file with cwd(correctworkingdirectory)
+logs_path = os.path.join(os.getcwd(),'logs', LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename = LOG_FILE_PATH,
+    format = '[%(asctime)s] %(lineno)d %(name)s - %(levelname)s -%(message)s',
+    level = logging.INFO,
+)
+
+
+if __name__ =="__main__":
+    logging.info("logs are imported")
+    
